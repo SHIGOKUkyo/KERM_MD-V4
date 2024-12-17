6ea18d3fcd8a714f6bb58b2b0f2a493c6b76a155
SHIGOKUkyo <ndingadidace@gmail.com>
2024-12-17 12:20:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+if (fs.existsSync('config.env')) require('dotenv').config({ path:'./config.env'});
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "nRIRVL6Z#IH5cCiLfYj-oufuspn2DSlC6InB4KO6ojR_YdsCXGJ8",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
